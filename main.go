1026d1a92e85e40fd96f55e35cea109631fa747b
Drew Wells <drew.wells00@gmail.com>
2014-08-25 12:26:16 -0500

    Code comments

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ type ImageList struct {
 	Vertical bool
 }
 
+// Return the X position of an image based
+// on the layout (vertical/horizontal) and
+// position in Image slice
 func (l ImageList) X(pos int) int {
 	x := 0
 	if !l.Vertical {
@@ -24,6 +27,9 @@ func (l ImageList) X(pos int) int {
 	return x
 }
 
+// Return the Y position of an image based
+// on the layout (vertical/horizontal) and
+// position in Image slice
 func (l ImageList) Y(pos int) int {
 	y := 0
 	for i := 0; i < pos; i++ {
@@ -32,6 +38,8 @@ func (l ImageList) Y(pos int) int {
 	return y
 }
 
+// Return the cumulative Height of the
+// image slice.
 func (l *ImageList) Height(sum bool) int {
 	h := 0
 	ll := *l
@@ -46,6 +54,8 @@ func (l *ImageList) Height(sum bool) int {
 	return h
 }
 
+// Return the cumulative Width of the
+// image slice.
 func (l *ImageList) Width(sum bool) int {
 	w := 0
 	ll := *l
@@ -60,6 +70,9 @@ func (l *ImageList) Width(sum bool) int {
 	return w
 }
 
+// Accept a variable number of image paths returning
+// an image slice of each file path decoded into a
+// *magick.Image.
 func (l *ImageList) Decode(rest ...string) {
 	ll := *l
 	for _, path := range rest {
@@ -72,6 +85,8 @@ func (l *ImageList) Decode(rest ...string) {
 	*l = ll
 }
 
+// Combine all images in the slice into a final output
+// image.
 func (l *ImageList) Combine(vertical bool) *magick.Image {
 	l.Vertical = vertical
 	var (
