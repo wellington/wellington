cc13f94c01a90b7d0ab77c934f971cea20891692
Drew Wells <drew.wells00@gmail.com>
2014-08-25 17:00:51 -0500

    Integrate gosass in

--- load_test.go
+++ load_test.go
@@ -7,15 +7,20 @@ import (
 	"github.com/moovweb/gosass"
 )
 
+func fileString(path string) string {
+	bytes, _ := ioutil.ReadFile(path)
+	return string(bytes)
+}
+
 func TestCompile(t *testing.T) {
 	ctx := Context{
 		Output: "test/file1.css",
-		Sass: &gosass.FileContext{
+		Sass: &gosass.Context{
 			Options: gosass.Options{
 				OutputStyle:  gosass.NESTED_STYLE,
 				IncludePaths: make([]string, 0),
 			},
-			InputPath:    "test/file1.scss",
+			SourceString: fileString("test/file1.scss"),
 			OutputString: "",
 		},
 	}
@@ -28,7 +33,7 @@ func TestCompile(t *testing.T) {
 		t.Errorf("file1.scss string mismatch found: \n%s, expected \n%s", ctx.Sass.OutputString, file1)
 	}
 
-	ctx.Sass.InputPath = "test/file1a.scss"
+	ctx.Sass.SourceString = fileString("test/file1a.scss")
 	ctx.Compile()
 
 	if ctx.Sass.OutputString != file1 {
@@ -39,12 +44,12 @@ func TestCompile(t *testing.T) {
 func TestExport(t *testing.T) {
 	ctx := Context{
 		Output: "test/sheet1.css",
-		Sass: &gosass.FileContext{
+		Sass: &gosass.Context{
 			Options: gosass.Options{
 				OutputStyle:  gosass.NESTED_STYLE,
 				IncludePaths: make([]string, 0),
 			},
-			InputPath:    "test/sheet1.scss",
+			SourceString: fileString("test/sheet1.scss"),
 			OutputString: "",
 		},
 	}
--- sass.go
+++ sass.go
@@ -1,23 +1,67 @@
 package main
 
+/*
+#cgo LDFLAGS: -Llibsass -lsass -lstdc++
+#cgo CFLAGS: -Ilibsass
+
+#include <stdlib.h>
+#include <sass_interface.h>
+*/
+import "C"
+
 import (
 	"log"
+	"strings"
+	"unsafe"
 
 	"github.com/moovweb/gosass"
 )
 
 type Context struct {
-	Sass          *gosass.FileContext
+	Sass          *gosass.Context
 	Sprites       []ImageList
 	Input, Output string
 }
 
 func (ctx Context) Compile() {
-	if ctx.Sass.InputPath == "" || ctx.Output == "" {
+	if ctx.Sass.SourceString == "" {
 		log.Fatal("No input/output file specified")
 	}
-	gosass.CompileFile(ctx.Sass)
+	libCompile(ctx.Sass)
 
+}
+
+// Constants/enums for the output style.
+const (
+	NESTED_STYLE = iota
+	EXPANDED_STYLE
+	COMPACT_STYLE
+	COMPRESSED_STYLE
+)
+
+func libCompile(goCtx *gosass.Context) {
+	// set up the underlying C context struct
+	cCtx := C.sass_new_context()
+	cCtx.source_string = C.CString(goCtx.SourceString)
+	defer C.free(unsafe.Pointer(cCtx.source_string))
+	cCtx.options.output_style = C.int(goCtx.Options.OutputStyle)
+	if goCtx.Options.SourceComments {
+		cCtx.options.source_comments = C.int(1)
+	} else {
+		cCtx.options.source_comments = C.int(0)
+	}
+	cCtx.options.include_paths = C.CString(strings.Join(goCtx.Options.IncludePaths, ":"))
+	defer C.free(unsafe.Pointer(cCtx.options.include_paths))
+	cCtx.options.image_path = C.CString(goCtx.Options.ImagePath)
+	defer C.free(unsafe.Pointer(cCtx.options.image_path))
+	// call the underlying C compile function to populate the C context
+	C.sass_compile(cCtx)
+	// extract values from the C context to populate the Go context object
+	goCtx.OutputString = C.GoString(cCtx.output_string)
+	goCtx.ErrorStatus = int(cCtx.error_status)
+	goCtx.ErrorMessage = C.GoString(cCtx.error_message)
+	// don't forget to free the C context!
+	C.sass_free_context(cCtx)
 }
 
 func (ctx Context) Export() {
