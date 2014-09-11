9f9cb3b1ed6d5347129ea25047ca58350862836f
Drew Wells <drew.wells00@gmail.com>
2014-09-11 09:30:18 -0500

    Split out importer to separate file

---
+++ import.go
@@ -0,0 +1,46 @@
+package sprite_sass
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+)
+
+func (p *Parser) ImportPath(dir, file string) (string, string, error) {
+	// fmt.Println("Importing: " + file)
+	baseerr := ""
+	//Load and retrieve all tokens from imported file
+	path, err := filepath.Abs(fmt.Sprintf("%s/%s.scss", dir, file))
+	if err != nil {
+		panic(err)
+	}
+	pwd := filepath.Dir(path)
+	// Sass put _ in front of imported files
+	fpath := pwd + "/_" + filepath.Base(path)
+	contents, err := ioutil.ReadFile(fpath)
+	if err == nil {
+		return pwd, string(contents), nil
+	}
+	baseerr += fpath + "\n"
+	if strings.HasSuffix(err.Error(), "no such file or directory") {
+		// Look through the import path for the file
+		for _, lib := range p.Includes {
+			path, err := filepath.Abs(lib + "/" + file)
+			if err != nil {
+				panic(err)
+			}
+			pwd := filepath.Dir(path)
+			fpath = pwd + "/_" + filepath.Base(path) + ".scss"
+			contents, err := ioutil.ReadFile(fpath)
+			baseerr += fpath + "\n"
+			if err == nil {
+				return pwd, string(contents), nil
+			}
+		}
+	}
+
+	return pwd, string(contents), errors.New("Could not import: " +
+		file + "\nTried:\n" + baseerr)
+}
--- parser.go
+++ parser.go
@@ -1,12 +1,10 @@
 package sprite_sass
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"path/filepath"
-	"strings"
 
 	// "github.com/kr/pretty"
 )
@@ -307,41 +305,4 @@ func (p *Parser) parser(pwd, input string) ([]Item, string, error) {
 			}
 		}
 	}
-}
-
-func (p *Parser) ImportPath(dir, file string) (string, string, error) {
-	// fmt.Println("Importing: " + file)
-	baseerr := ""
-	//Load and retrieve all tokens from imported file
-	path, err := filepath.Abs(fmt.Sprintf("%s/%s.scss", dir, file))
-	if err != nil {
-		panic(err)
-	}
-	pwd := filepath.Dir(path)
-	// Sass put _ in front of imported files
-	fpath := pwd + "/_" + filepath.Base(path)
-	contents, err := ioutil.ReadFile(fpath)
-	if err == nil {
-		return pwd, string(contents), nil
-	}
-	baseerr += fpath + "\n"
-	if strings.HasSuffix(err.Error(), "no such file or directory") {
-		// Look through the import path for the file
-		for _, lib := range p.Includes {
-			path, err := filepath.Abs(lib + "/" + file)
-			if err != nil {
-				panic(err)
-			}
-			pwd := filepath.Dir(path)
-			fpath = pwd + "/_" + filepath.Base(path) + ".scss"
-			contents, err := ioutil.ReadFile(fpath)
-			baseerr += fpath + "\n"
-			if err == nil {
-				return pwd, string(contents), nil
-			}
-		}
-	}
-
-	return pwd, string(contents), errors.New("Could not import: " +
-		file + "\nTried:\n" + baseerr)
 }
