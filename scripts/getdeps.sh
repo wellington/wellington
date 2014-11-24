f2b1d06702dda1175765e3d38b09e8e2ec870954
Drew Wells <drew.wells00@gmail.com>
2014-11-24 15:29:00 -0600

    Whats up wercker no like [[

--- scripts/getdeps.sh
+++ scripts/getdeps.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-HASH="85bccf"
+HASH=$(cat libsass/.lib_version)
 
 [ -d libsass ] || mkdir libsass
-
-if [[ ! -f libsass/"libsass.$HASH.tar.gz" ]];
+if [ -f libsass/"libsass.$HASH.tar.gz" ];
 then
+	echo "Cache found $HASH"
+else
+	echo "Fetching source of $HASH"
 	cd libsass
 	curl -L "https://github.com/sass/libsass/archive/$HASH.tar.gz" -o "libsass.$HASH.tar.gz"
 	tar xvf "libsass.$HASH.tar.gz" --strip 1
-	sudo make install
 	cd ..
+fi
+# Check file permissions
+if [ -w '/usr/local/lib/libsass.a' ];
+then
+	cd libsass; make install
+else
+	cd libsass; sudo make install
 fi
