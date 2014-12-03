35c8223114bdf85792f9bbedbee1e23d0ea396e8
Drew Wells <drew.wells00@gmail.com>
2014-12-03 14:31:58 -0600

    Turn on vet

--- scripts/goclean.sh
+++ scripts/goclean.sh
@@ -11,10 +11,10 @@
 set -e
 
 # Automatic checks
-#test -z "$(gofmt -l -w .     | tee /dev/stderr)"
-#test -z "$(goimports -l -w . | tee /dev/stderr)"
+test -z "$(gofmt -l -w .     | tee /dev/stderr)"
+test -z "$(goimports -l -w . | tee /dev/stderr)"
 #test -z "$(golint .          | tee /dev/stderr)"
-#go vet ./...
+go vet ./...
 go test -race ./...
 
 # Run test coverage on each subdirectories and merge the coverage profile.
