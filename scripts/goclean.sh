2170515a8ea3d9209853a8a81128102dc3408016
Drew Wells <drew.wells00@gmail.com>
2014-12-20 09:45:04 -0600

    circle builds with testing, coverage and artifacts

--- scripts/goclean.sh
+++ scripts/goclean.sh
@@ -25,7 +25,8 @@ echo "mode: count" > profile.cov
 for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d);
 do
 if ls $dir/*.go &> /dev/null; then
-    go test -covermode=count -coverprofile=$dir/profile.tmp $dir
+	echo $dir
+	go test -covermode=count -coverprofile=$dir/profile.tmp $dir
     if [ -f $dir/profile.tmp ]
     then
         cat $dir/profile.tmp | tail -n +2 >> profile.cov
