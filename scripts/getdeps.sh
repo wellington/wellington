c99c5d9e34f9a303c12d9d5561febb3455b7acd7
Drew Wells <drew.wells00@gmail.com>
2015-01-04 23:38:38 -0600

    support redirects and move pkg config path

--- scripts/getdeps.sh
+++ scripts/getdeps.sh
@@ -7,10 +7,10 @@ if [ -f libsass/"libsass.$HASH.tar.gz" ];
 then
 	echo "Cache found $HASH"
 else
-	echo "Fetching source of $HASH"
+	echo "Fetching source of https://github.com/drewwells/libsass/archive/$HASH.tar.gz"
 	cd libsass
-	make clean
-	curl -L "https://github.com/drewwells/libsass/archive/$HASH.tar.gz" -o "libsass.$HASH.tar.gz"
+	#make clean
+	curl -s -S -L "https://github.com/sass/libsass/archive/$HASH.tar.gz" -o "libsass.$HASH.tar.gz"
 	tar xvf "libsass.$HASH.tar.gz" --strip 1
 	cd ..
 fi
