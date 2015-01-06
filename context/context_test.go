ec51161468037d98c0c4d648d1e2d1600a09f981
Drew Wells <drew.wells00@gmail.com>
2015-01-05 21:20:39 -0600

    remove funcs used no where

--- context/context_test.go
+++ context/context_test.go
@@ -4,45 +4,11 @@ import (
 	"bytes"
 	"fmt"
 	"image/color"
-	"io"
-	"io/ioutil"
-	"log"
 	"os"
-	"path/filepath"
 	"reflect"
 	"regexp"
 	"testing"
-
-	"github.com/wellington/spritewell"
 )
-
-func cleanUpSprites(sprites map[string]spritewell.ImageList) {
-	if sprites == nil {
-		return
-	}
-	for _, iml := range sprites {
-		err := os.Remove(filepath.Join(iml.GenImgDir, iml.OutFile))
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func fileString(path string) string {
-	bytes, err := ioutil.ReadFile(path)
-	if err != nil {
-		panic(err)
-	}
-	return string(bytes)
-}
-
-func fileReader(path string) io.Reader {
-	reader, err := os.Open(path)
-	if err != nil {
-		panic(err)
-	}
-	return reader
-}
 
 var rerandom *regexp.Regexp
 
--- setup_test.go
+++ setup_test.go
@@ -2,33 +2,8 @@ package wellington
 
 import (
 	"io"
-	"io/ioutil"
-	"log"
 	"os"
-	"path/filepath"
-
-	. "github.com/wellington/spritewell"
 )
-
-func cleanUpSprites(sprites map[string]ImageList) {
-	if sprites == nil {
-		return
-	}
-	for _, iml := range sprites {
-		err := os.Remove(filepath.Join(iml.GenImgDir, iml.OutFile))
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func fileString(path string) string {
-	bytes, err := ioutil.ReadFile(path)
-	if err != nil {
-		panic(err)
-	}
-	return string(bytes)
-}
 
 func fileReader(path string) io.Reader {
 	reader, err := os.Open(path)
