759007ba75942e37bb038f8bb90053ea3fb7d0cc
Drew Wells <drew.wells00@gmail.com>
2015-02-17 02:29:26 -0600

    :boom: add base.scss testing

--- spec_test.go
+++ spec_test.go
@@ -2,7 +2,7 @@ package wellington
 
 import "testing"
 
-func TestProcessSass(t *testing.T) {
+func TestSassToScss(t *testing.T) {
 
 	p := NewParser()
 	p.Includes = []string{"test/whitespace"}
@@ -27,6 +27,29 @@ $primary-color: #333;
 body {
   font: 100% $font-stack;
   background-color: $primary-color; }
+`
+
+	if e != string(bs) {
+		t.Errorf("got:\n%s\nwanted:\n%s", string(bs), e)
+	}
+
+	bs, err = p.Start(fileReader("test/whitespace/base.sass"), "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e = `@mixin sprite-dimensions($map, $name) {
+  $file: sprite-file($map, $name);
+  height: image-height($file);
+  width: image-width($file);
+}
+
+html,
+body,
+ul,
+ol {
+  margin:  0;
+  padding: 0; }
 `
 
 	if e != string(bs) {
