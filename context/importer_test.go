11c24e7c9d1895ecd725bf59dfa4699b286d378a
Drew Wells <drew.wells00@gmail.com>
2015-02-23 08:27:44 -0600

    nesting creates an out of order situation

--- context/importer_test.go
+++ context/importer_test.go
@@ -48,3 +48,34 @@ b {
 	}
 
 }
+
+func TestSassImporter_nested(t *testing.T) {
+	in := bytes.NewBufferString(`@import "branch";
+div.branch {
+  @extend %branch;
+  div.leaf {
+    @extend %leaf;
+  }
+}`)
+
+	var out bytes.Buffer
+	ctx := Context{}
+
+	ctx.AddImport("branch", `@import "leaf";
+%branch { color: brown; }`)
+	ctx.AddImport("leaf", "%leaf { color: green; }")
+	err := ctx.Compile(in, &out)
+	if err != nil {
+		t.Fatal(err)
+	}
+	e := `div.branch div.leaf {
+  color: green; }
+
+div.branch {
+  color: brown; }
+
+`
+	if e != out.String() {
+		t.Fatalf("got:\n%s\nwanted:\n%s", out.String(), e)
+	}
+}
