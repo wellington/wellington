ae0fa5f7dfe237ce30505f2e08563e42f03e92f0
Drew Wells <drew.wells00@gmail.com>
2015-06-22 01:54:44 -0500

    arch specific cgo ldl flags

--- Godeps/_workspace/src/github.com/wellington/go-libsass/export.go
+++ Godeps/_workspace/src/github.com/wellington/go-libsass/export.go
@@ -8,7 +8,8 @@ package context
 // #cgo CFLAGS: -O2 -fPIC
 // #cgo CPPFLAGS: -w
 // #cgo CXXFLAGS: -g -std=c++0x -pedantic -Wno-c++11-extensions -O2 -fPIC
-// #cgo LDFLAGS: -lstdc++ -ldl -lm
+// #cgo darwin linux LDFLAGS: -ldl
+// #cgo LDFLAGS: -lstdc++ -lm
 // #include "sass_context.h"
 //
 import "C"
