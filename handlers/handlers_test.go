cc57c70e8266ae9209bb5f2e7360e20d2982083b
Drew Wells <drew.wells00@gmail.com>
2015-08-15 13:01:05 -0500

    test cleanup

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"strings"
 	"testing"
 	"time"
 
@@ -411,58 +410,6 @@ div {
 		t.Errorf("got:\n%s\nwanted:\n%s", err.Error(), e)
 	}
 
-}
-
-func TestFontURLFail(t *testing.T) {
-	r, w, _ := os.Pipe()
-	_ = r
-	old := os.Stdout
-	defer func() { os.Stdout = old }()
-	os.Stdout = w
-	in := bytes.NewBufferString(`@font-face {
-  src: font-url("arial.eot");
-}`)
-	var out bytes.Buffer
-	ctx := libsass.Context{}
-	err := ctx.Compile(in, &out)
-
-	e := "error in C function font-url: font-url: font path not set"
-	if !strings.Contains(err.Error(), e) {
-		t.Errorf("got:\n%s\nwanted:\n%s\n", err, e)
-	}
-
-	// Removed this as part of making font-url fail instead of
-	// output garbage
-	//
-	// outC := make(chan string)
-	// go func(r *os.File) {
-	// 	var buf bytes.Buffer
-	// 	io.Copy(&buf, r)
-	// 	outC <- buf.String()
-	// }(r)
-
-	// w.Close()
-	// stdout := <-outC
-
-}
-
-func ExampleFontURL() {
-	in := bytes.NewBufferString(`
-$path: font-url("arial.eot", true);
-@font-face {
-  src: font-url("arial.eot");
-  src: url("#{$path}");
-}`)
-
-	_, _, err := setupCtx(in, os.Stdout)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// Output:
-	// @font-face {
-	//   src: url("../font/arial.eot");
-	//   src: url("../font/arial.eot"); }
 }
 
 func ExampleMappedArguments() {
