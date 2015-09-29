515ca5a42ac434c78ce564b3f94ff19d6eeb9060
Drew Wells <drew.wells00@gmail.com>
2015-09-28 22:28:54 -0500

    pather finds scss and sass top files

---
+++ pather.go
@@ -0,0 +1,68 @@
+package wellington
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func pathsToFiles(paths []string, recurse bool) []string {
+	var rollup []string
+	for _, path := range paths {
+		if recurse {
+			paths, err := recursePath(path)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			rollup = append(rollup, paths...)
+		} else {
+			rollup = append(rollup, resolvePath(path)...)
+		}
+	}
+	return rollup
+}
+
+var resolveExts = []string{".scss", ".sass"}
+
+func isImportable(name string) bool {
+	ext := filepath.Ext(name)
+
+	var match bool
+	for _, r := range resolveExts {
+		if ext == r {
+			match = true
+		}
+	}
+
+	// remove partials
+	return match && !strings.HasPrefix(name, "_")
+}
+
+// recursePath takes an input path and locates all non-partial scss/sass
+// files in it
+func recursePath(path string) ([]string, error) {
+	var paths []string
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() && isImportable(info.Name()) {
+			paths = append(paths, path)
+		}
+		return nil
+	})
+	return paths, err
+}
+
+//
+func resolvePath(path string) []string {
+	var paths []string
+	for _, r := range resolveExts {
+		some, err := filepath.Glob(filepath.Join(path, "*"+r))
+		if err != nil {
+			continue
+		}
+		paths = append(paths, some...)
+	}
+
+	return paths
+}
